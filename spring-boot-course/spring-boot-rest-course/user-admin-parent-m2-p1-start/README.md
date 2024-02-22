571d49397f61c5285f88ab18e31b13873b30930b
Ricardo Yanez <ricardo.yanezv@circulodecredito.com.mx>
2024-02-21 22:58:47 -0600

    push module2

---
+++ spring-boot-course/spring-boot-rest-course/user-admin-parent-m2-p1-start/README.md
@@ -0,0 +1,2 @@
+# Module 2 -  This is the codebase for Module 1, Pratice 1
+For more details, check the source code.
---
+++ spring-boot-course/spring-boot-rest-course/user-admin-parent-m2-p1-start/user-service/src/test/java/com/apixcloud/users/userservicem2p1start/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apixcloud.users.userservicem2p1start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
