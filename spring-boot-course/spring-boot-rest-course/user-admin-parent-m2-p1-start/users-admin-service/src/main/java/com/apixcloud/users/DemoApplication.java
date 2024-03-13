3d994eaedb58af64459b8a2d667767c4533e9f9f
Ricardo Yanez <ricardo.yanezv@circulodecredito.com.mx>
2024-03-12 22:37:28 -0600

    webinar

--- spring-boot-course/spring-boot-rest-course/user-admin-parent-m2-p1-start/users-admin-service/src/main/java/com/apixcloud/users/DemoApplication.java
+++ spring-boot-course/spring-boot-rest-course/user-admin-parent-m2-p1-start/users-admin-service/src/main/java/com/apixcloud/users/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.apixcloud.usersadminservicem2p1start;
+package com.apixcloud.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
